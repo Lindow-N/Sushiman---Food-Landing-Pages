02be00adb804912f48b9eecdcfbc39b9e1af29fb
pok <yest@gmail.com>
2022-08-20 12:48:06 +0200

    Configure React app for deployment to GitHub Pages2

--- src/Main/App.js
+++ src/Main/App.js
@@ -7,7 +7,7 @@ import menu from "../Asset/iconmonstr-menu-lined-240.png";
 import fond1 from "../Asset/Desktop - 1 (2).png";
 import Sushi from "../Asset/iMac - 1.png";
 import saumon from "../Asset/iMac - 2.png";
-import Avatar from "../Asset/94041140-femme-asiatique-avatar-icône-femme-japonaise-dans-le-profil-traditionnel-profil-plat-illustration-ve.webp";
+import Avatar from "../Asset/94041140-femme-asiatique.webp";
 import "./App.css";
 
 function App() {
